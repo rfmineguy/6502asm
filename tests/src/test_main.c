a5ca1e7a7268102e05bedf670534af8767087a42
Riley Fischer <rfmineguy@gmail.com>
2025-12-27 10:16:56 -0800

    tests: write build script and add parse_number_test as first test in suite

--- tests/src/test_main.c
+++ tests/src/test_main.c
@@ -1,6 +1,12 @@
 #include "tests.h"
 
+MunitTest parse_test[] = {
+  {"/number", parse_number_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+  {NULL, NULL, NULL, 0, MUNIT_TEST_OPTION_NONE},
+};
+
 MunitSuite suite = {
+  "/parse", parse_test, NULL, 1, MUNIT_SUITE_OPTION_NONE
 };
 
 int main(int argc, char** argv) {
