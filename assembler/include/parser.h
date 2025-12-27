9d6365d6d6a166bbea174f30da921ba5adefb3c2
Riley Fischer <rfmineguy@gmail.com>
2025-12-27 11:41:59 -0800

    parser: rework parser header to logically separate public from private functions

--- assembler/include/parser.h
+++ assembler/include/parser.h
@@ -1,5 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
+#include <stdbool.h>
 
 typedef struct {
 } instruction;
@@ -7,9 +8,17 @@ typedef struct {
 typedef struct {
 } parse_ctx;
 
-const char* parse_number(const char* cursor, long* num_out);
-int         asciilen(const char* cursor);
-
+// Public Interface
 const char* parse_line(const char*, instruction*);
+
+// Private Interface
+#ifdef ENABLE_PRIVATE_INTERFACE
+const char* parse_number(const char* cursor, long* num_out);
+const char* parse_org(const char* cursor, long* addr_out);
+const char* parse_next_line(const char* cursor);
+
+int         alphalen(const char* cursor);
+bool        isnewline(const char* s, int* len);
+#endif
 
 #endif
