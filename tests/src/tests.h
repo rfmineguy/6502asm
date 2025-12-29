9c226e018ba50c7cb831d0a067659323ce92dd1e
Riley Fischer <rfmineguy@gmail.com>
2025-12-28 20:20:32 -0800

    test: add isnewline to test suite

--- tests/src/tests.h
+++ tests/src/tests.h
@@ -7,3 +7,12 @@ MunitResult parser_parse_org_test(const MunitParameter *params, void *fixture);
 MunitResult parse_util_parse_number_test(const MunitParameter params[], void* fixture);
 MunitResult parse_util_alphalen_test(const MunitParameter *params, void *fixture);
 MunitResult parse_util_isnewline_test(const MunitParameter *params, void *fixture);
+
+MunitResult parse_operand_immediate_test(const MunitParameter *params, void *fixture);
+MunitResult parse_operand_zp_test       (const MunitParameter *params, void *fixture);
+MunitResult parse_operand_zpx_test      (const MunitParameter *params, void *fixture);
+MunitResult parse_operand_absolute_test (const MunitParameter *params, void *fixture);
+MunitResult parse_operand_absolutex_test(const MunitParameter *params, void *fixture);
+MunitResult parse_operand_absolutey_test(const MunitParameter *params, void *fixture);
+MunitResult parse_operand_indirectx_test(const MunitParameter *params, void *fixture);
+MunitResult parse_operand_indirecty_test(const MunitParameter *params, void *fixture);
